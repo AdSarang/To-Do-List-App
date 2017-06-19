ce7c031d68cbb5bd81ee1a173990398550d7b8d7
Advait <advait@advaits-macbook-pro.local>
2017-06-19 22:17:04 +0530

    Version 1.0

--- To Do List/FirstViewController.swift
+++ To Do List/FirstViewController.swift
@@ -8,11 +8,27 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController,UITableViewDataSource,UITableViewDelegate,UITabBarDelegate {
 
+    var items = [String]()
+    @IBOutlet weak var tableList: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+   
+        
+        let itemsObject = UserDefaults.standard.value(forKey: "items")
+        
+        if let temp = itemsObject as? [String]{
+            
+            items = temp
+        }
+        
+        tableList.reloadData()
+    }
+    
+    func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        tableList.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +36,43 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = items[indexPath.row]
 
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let itemsObject = UserDefaults.standard.object(forKey: "items")
+        
+        
+        if let tempItems = itemsObject as? [String] {
+            
+            items = tempItems
+            
+        }
+        
+        tableList.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.delete{
+           items.remove(at: indexPath.row)
+            
+            UserDefaults.standard.set(items, forKey: "items")
+            
+            tableList.reloadData()
+        }
+        
+    }
+    
 }
 
--- To Do List/SecondViewController.swift
+++ To Do List/SecondViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    
+    
+    
+    
+    @IBOutlet weak var resultOutputLabel: UILabel!
+    @IBOutlet weak var userInputItem: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,34 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func addItemButtonPressed(_ sender: Any) {
+        
+        
+        
+        let itemsObject = UserDefaults.standard.object(forKey: "items")
+        
+        var items = [String]()
+        
+        if let temp = itemsObject as? [String]{
+            items = temp
+            items.append(userInputItem.text!)
+            
+        }
+        else{
+            items=[userInputItem.text!]
+        }
+        
+        UserDefaults.standard.set(items, forKey: "items")
+        
+        resultOutputLabel.text = "\(userInputItem.text!) is added to List!"
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+        
+    }
+   
 
 }
 
